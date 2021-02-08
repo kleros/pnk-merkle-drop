84b4a40df9f30c2e9377f599b5f981e7b3e523a3
Henrique Barcelos <hbarcelos909@gmail.com>
2021-02-08 20:35:59 -0300

    feat: added deploy script for MerkleRedeem

---
+++ contracts/deploy/01-merkleRedeem.js
@@ -0,0 +1,24 @@
+const paramsByChainId = {
+  42: {
+    pnkAddress: "0x1EE318dBC19267dBCE08F54A66ab198F73EdE356",
+  },
+  1: {
+    pnkAddress: "0x93ED3FBe21207Ec2E8f2d3c3de6e058Cb73Bc04d",
+  },
+};
+
+module.exports = async function deployMerkleRedeem({ deployments, getNamedAccounts, getChainId }) {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const chainId = await getChainId();
+  const { pnkAddress } = paramsByChainId[chainId];
+
+  const merkleRedeem = await deploy("MerkleRedeem", {
+    from: deployer,
+    gas: 8000000,
+    args: [pnkAddress],
+  });
+
+  console.log("Deployed to:", merkleRedeem.address);
+};
